55365fc929851d9bf95222a540f4c7063eeef437
Mustapha-Amini <physics.amini@gmail.com>
2023-08-15 17:14:41 +0330

    +Using ActionResult instead of JsonResult

--- API-Playground/Controllers/CitiesController.cs
+++ API-Playground/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿
+using API_Playground.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
@@ -9,17 +10,24 @@ namespace API_Playground.Controllers
     public class CitiesController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetCities()
+        public ActionResult<IEnumerable<CitiesData>> GetCities()
         {
-            return new JsonResult(CitiesDataStore.current.Cities);
+            return Ok (CitiesDataStore.current.Cities);
 
         }
         [HttpGet("{id}")]
-        public JsonResult GetCity(int id) {
-            return new JsonResult(
-                CitiesDataStore.current.Cities.FirstOrDefault(c => c.CitiesDataID == id));
-               
-        }
+        public ActionResult<CitiesData> GetCity(int id) {
+            
+            var CityToReturn =   
+                CitiesDataStore.current.Cities.FirstOrDefault(c => c.CitiesDataID == id);
+
+            if (CityToReturn == null)
+            {
+              return NotFound();
+            }
+                
+            return Ok (CityToReturn);
+        }   
     }
 }
 
